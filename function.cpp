a33dbd0fa1efc664c01f8dd14ae1e01692e2036b
Nabegh <nabeghmuhra@nais.co.za>
2022-12-01 11:31:51 +0200

    repo

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// I created a function that prints "Hello!".
+void sayHi()
+{
+  cout << "Hello, World!\n";
+}
+
+int main() 
+{
+  // in order for the function to work I need to call it in the main function.
+  sayHi();
+  
+  return 0;
+}
